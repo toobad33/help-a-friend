dc0137263cf5cd115c494418c003cacfc5bb3712
Jefferson Lam <jefferson.lam@polymtl.ca>
2020-01-18 18:02:43 -0500

    Working Google Cloud NLP

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const express      = require("express"),
-      bodyParser   = require("body-parser");
+const app = require('express')();
 
-const app = express();
+async function quickstart() {
+    // Imports the Google Cloud client library
+    const language = require('@google-cloud/language');
+  
+    // Instantiates a client
+    const client = new language.LanguageServiceClient();
+  
+    // The text to analyze
+    const text = "J'ai tellement envi de me suicider, c'est pas croyable.";
+  
+    const document = {
+      content: text,
+      type: 'PLAIN_TEXT',
+    };
+  
+    // Detects the sentiment of the text
+    const [result] = await client.analyzeSentiment({document: document});
+    const sentiment = result.documentSentiment;
+  
+    console.log(`Text: ${text}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  }
+  
+quickstart();
 
 app.get("/", (req, res) => {
     res.send("give me a form baby");
