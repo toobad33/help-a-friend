f234d7bdd26e77b7f044f616055741f4e4402c2e
Jefferson Lam <jefferson.lam@polymtl.ca>
2020-01-18 22:33:26 -0500

    Adding authent

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const app = require('express')();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const app           = require('express')();
+const http          = require('http').createServer(app);
+const io            = require('socket.io')(http);
+const mongoose      = require("mongoose");
+const passport      = require("passport");
+const LocalStrategy = require("passport-local");
+const User          = require("./models/user");
+
+let connections = [];
+
+mongoose.connect("mongodb://localhost:27017/help-a-friend", { useUnifiedTopology: true, useNewUrlParser: true });
+
+app.use(require("express-session")({
+    secret: "We must care more about mental disease.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 async function quickstartNLP(message) {
     // Imports the Google Cloud client library
@@ -26,7 +46,35 @@ async function quickstartNLP(message) {
     console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
   }
   
-quickstartNLP("J'ai envi de me suicider");
+quickstartNLP("J'ai envie de me suicider");
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+})
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"}),
+    (req, res) => {}    
+);
+
+app.get("/register", (req, res)=>{
+    res.sendFile(__dirname + '/register.html');
+});
+
+app.post("/register", (req, res)=>{
+    let newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.sendFile(__dirname + "/register.html");
+        } 
+        passport.authenticate("local")(req, res, function(){
+            console.log("Succesfully logged in.");
+            res.redirect("/");
+        })
+    });
+});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -35,24 +83,21 @@ app.get("/", (req, res) => {
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+io.sockets.on('connection', (socket) => {
+    //Connections
+    connections.push(socket);
+    console.log('Connected : %s sockets connected', connections.length);
 
-    // socket.on('chat message', (msg) => {
-    //     console.log('message: '  + msg);
-    //     quickstartNLP(msg);
-    // });
-
-    io.on('connection', function(socket){
-        socket.on('chat message', function(msg){
-            console.log('message: ' + msg);
-            quickstartNLP(msg);
-            io.emit('chat message', msg);
-          });        
-    });      
-
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        quickstartNLP(msg);
+        io.emit('chat message', msg);
+    });        
+    
+    //Disconnect
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
     });
 });
 
